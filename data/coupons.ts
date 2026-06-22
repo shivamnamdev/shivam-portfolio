3e52205cb9060c18aa7b68c91700d43aa439cefa
shivamnamdevqa-kyndryl <shivam.namdev@kyndryl.com>
2026-06-22 07:16:44 +0530

    Revert "Update call link and coupon"
    This reverts commit e29dda04d96caa81028873e885489de8019f5c8b.

--- data/coupons.ts
+++ data/coupons.ts
@@ -2,7 +2,7 @@
 
 export const activeCoupons =[
   {
-    code: "LASTCHANCE26",
+    code: "pppprrr55416239217649238749",
     discountType: "fixed", // 'percentage' or 'fixed'
     discountValue: {
       inr: 799, // ₹200 off for India
