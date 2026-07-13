38cc8aaeafb66eec70d4d160ba9079767bf8d214
shivamnamdev <shivamnamdev.edu@gmail.com>
2026-07-13 09:18:02 +0530

    update Git Workflow 2 updated

--- data/learning-content.ts
+++ data/learning-content.ts
@@ -663,7 +663,8 @@ export const courseCurriculumMap: Record<string, any[]> = {
   {
     moduleTitle: "Module 5: Industry Workflow",
     videoIds:[
-      "iU9ZGojVVgU"
+      "iU9ZGojVVgU",
+      "FjndEmsNTEU"
     ], 
     githubAssignments: {
     "WhP2F-xk3Kk": {
